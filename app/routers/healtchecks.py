8660dc3f1b7ee905aefee1559f162801ce40a7f1
Aditya Ghaffar <adit.bisa@gmail.com>
2022-10-02 21:01:37 +0700

    feat: healthcheck database connection

--- app/routers/healtchecks.py
+++ app/routers/healtchecks.py
@@ -1,15 +1,17 @@
 from fastapi import APIRouter, status
 
+from app.database import engine
+
 router = APIRouter(prefix="/healthchecks")
 
 
-@router.get(
-    "/",
-    status_code=status.HTTP_200_OK,
-    tags=["health"]
-)
+@router.get("/", status_code=status.HTTP_200_OK, tags=["health"])
 async def readiness():
     """
     Readiness healthcheck endpoint.
     """
+
+    with engine.connect() as connection:
+        connection.execute("SELECT 1")
+
     return "OK 👍"
