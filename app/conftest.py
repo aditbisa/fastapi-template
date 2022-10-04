c4883a590e1c66c3f6dbefe8e604cbe7c2a39656
Aditya Ghaffar <adit.bisa@gmail.com>
2022-10-04 14:12:27 +0700

    fix: patch env var for settings

--- app/conftest.py
+++ app/conftest.py
@@ -1,12 +1,9 @@
 from typing import Generator
 
-import dotenv
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import MetaData
 from sqlalchemy.orm import Session
-
-from app.main import app
 
 
 @pytest.fixture
@@ -14,6 +11,8 @@ def test_client() -> TestClient:
     """
     Mock FastAPI client.
     """
+    from app.main import app
+
     client = TestClient(app)
     return client
 
@@ -24,8 +23,6 @@ def session(monkeypatch) -> Generator[Session, None, None]:
     Mock session with test database.
     """
     monkeypatch.setenv("DB_DATABASE", "test_db")
-    dotenv_file = dotenv.find_dotenv()
-    dotenv.set_key(dotenv_file, "DB_DATABASE", "test_db")
 
     from app.database import SessionLocal, engine
     from app.models import Base
