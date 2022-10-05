007aa4477e78ce3ee3a4f2e1ccecfd7e3b9d4b6d
Aditya Ghaffar <adit.bisa@gmail.com>
2022-10-05 13:53:11 +0700

    fix: patch envvar for test

--- app/conftest.py
+++ app/conftest.py
@@ -1,9 +1,21 @@
+import os
 from typing import Generator
+from unittest.mock import patch
 
 import pytest
+from dotenv import dotenv_values
 from fastapi.testclient import TestClient
 from sqlalchemy import MetaData
 from sqlalchemy.orm import Session
+
+# Patch environment variable without specify test env_file in pydantic config
+envs = dotenv_values(".env")
+envs.update({"DB_DATABASE": "test_db"})
+patch.dict(os.environ, envs).start()
+
+from app.database import SessionLocal, engine  # noqa: E402
+from app.main import app  # noqa: E402
+from app.models import Base  # noqa: E402
 
 
 @pytest.fixture
@@ -11,22 +23,15 @@ def test_client() -> TestClient:
     """
     Mock FastAPI client.
     """
-    from app.main import app
-
     client = TestClient(app)
     return client
 
 
 @pytest.fixture
-def session(monkeypatch) -> Generator[Session, None, None]:
+def session() -> Generator[Session, None, None]:
     """
     Mock session with test database.
     """
-    monkeypatch.setenv("DB_DATABASE", "test_db")
-
-    from app.database import SessionLocal, engine
-    from app.models import Base
-
     metadata: MetaData = Base.metadata
     metadata.bind = engine
     metadata.drop_all()
