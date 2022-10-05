2fddf4c88047834d570d9903eeeed8684d663bf6
Aditya Ghaffar <adit.bisa@gmail.com>
2022-10-05 14:32:06 +0700

    feat: token endpoint

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 
-from app.routers import healtchecks, playgrounds
+from app.routers import authentications, healtchecks, playgrounds
 
 app = FastAPI(
     title="Rest API template",
 )
+app.include_router(authentications.router)
 app.include_router(healtchecks.router)
 app.include_router(playgrounds.router)
---
+++ app/routers/authentications.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
+from app.database import get_db
+from app.schemas.authentications import Token
+from app.services.authentications import authenticate_user, create_access_token
+
+router = APIRouter()
+
+
+@router.post(
+    "/token",
+    response_model=Token,
+)
+def login(
+    db: Session = Depends(get_db),
+    payload: OAuth2PasswordRequestForm = Depends(),
+):
+    """
+    Login endpoint.
+    """
+    user = authenticate_user(db, payload.username, payload.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token = create_access_token(user)
+    return Token(access_token=access_token)
