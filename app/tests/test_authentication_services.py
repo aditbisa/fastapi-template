d68fee4c1d83908f1650dc89c264ff887d0cb953
Aditya Ghaffar <adit.bisa@gmail.com>
2022-10-05 14:30:57 +0700

    test: authentication services

---
+++ app/tests/test_authentication_services.py
@@ -0,0 +1,55 @@
+import pytest
+from sqlalchemy.orm import Session
+
+from app.models import UserModel
+from app.services.authentications import (
+    authenticate_user,
+    create_access_token,
+    verify_access_token,
+)
+
+
+def test_authenticate_user_success(session: Session, user_entry: UserModel):
+    """
+    Test authenticate_user function from services.
+    """
+    user = authenticate_user(session, "username", "password")
+    assert user == user_entry
+
+
+@pytest.mark.parametrize(
+    "username, password",
+    [
+        ("user-name", "password"),
+        ("username", "pass-word"),
+        ("aditya", "secret"),
+    ],
+)
+def test_authenticate_user_failed(session: Session, username: str, password: str):
+    """
+    Test authenticate_user function from services.
+    """
+    assert authenticate_user(session, username, password) is False
+
+
+def test_create_access_token_and_verify_access_token(user_entry: UserModel):
+    """
+    Test create_access_token and verify_access_token function from services.
+    """
+    access_token = create_access_token(user_entry)
+    user_id = verify_access_token(access_token)
+    assert user_id == user_entry.id
+
+
+@pytest.mark.parametrize(
+    "access_token",
+    [
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwia"
+        "WF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+    ],
+)
+def test_verify_access_token(access_token: str):
+    """
+    Test verify_access_token function from services.
+    """
+    assert verify_access_token(access_token) is False
