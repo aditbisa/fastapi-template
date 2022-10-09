ae8a41c149f5712b9a482e0abda1681933466611
Aditya Ghaffar <adit.bisa@gmail.com>
2022-10-09 15:19:25 +0700

    fix: url endpoint

--- app/tests/test_healthchecks.py
+++ app/tests/test_healthchecks.py
@@ -1,9 +1,9 @@
 from fastapi.testclient import TestClient
 
 
-def test_readiness_healthcheck(test_client: TestClient):
+def test_healthcheck_readiness(test_client: TestClient):
     """Test /healthchecks/ GET endpoint."""
 
-    response = test_client.get(f"/healthchecks/")
+    response = test_client.get("/healthchecks/readiness")
     assert response.status_code == 200
     assert response.json() == "OK 👍"
