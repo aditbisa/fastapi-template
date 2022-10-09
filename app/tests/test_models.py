fe7968c171eea9e70010043f124feffebe6fe41c
Aditya Ghaffar <adit.bisa@gmail.com>
2022-10-09 21:39:37 +0700

    test: roles and permissions

--- app/tests/test_models.py
+++ app/tests/test_models.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 
+from app.models.roles import PermissionModel, RoleModel
 from app.models.users import UserModel
 
 
@@ -20,3 +21,25 @@ def test_user_model(session: Session):
     assert created_user.password != "user-password"
     assert created_user.short_name == "Adit"
     assert created_user.verify_password("user-password") == 1
+
+
+def test_role_permissions_model(session: Session):
+    """
+    Test RoleModel and PermissionModel.
+    """
+    manager_role = RoleModel(title="Store Manager")
+
+    can_view_store_reports = PermissionModel(action="reports.store")
+    can_manage_store_users = PermissionModel(action="users.manage")
+    can_rectify_store_sales = PermissionModel(action="sales.rectify")
+    manager_role.permissions.append(can_view_store_reports)
+    manager_role.permissions.append(can_manage_store_users)
+    manager_role.permissions.append(can_rectify_store_sales)
+
+    session.add(manager_role)
+    session.commit()
+
+    created_role: RoleModel = session.query(RoleModel).one()
+    assert created_role.title == "Store Manager"
+    permissions = [p.action for p in created_role.permissions]
+    assert set(permissions) == set(["reports.store", "users.manage", "sales.rectify"])
