68fcaa25b459bb65be6d7db31bc97101aeb10fc2
Aditya Ghaffar <adit.bisa@gmail.com>
2022-11-27 16:24:59 +0700

    feat: user info with role

--- app/schemas/authentications.py
+++ app/schemas/authentications.py
@@ -1,4 +1,9 @@
+from typing import List, Optional
+
+from pydantic import Field, root_validator
+
 from app.schemas import BaseSchema
+from app.user_roles import USER_PERMISSIONS, UserAction
 
 
 class Token(BaseSchema):
@@ -17,3 +22,11 @@ class UserInfo(BaseSchema):
 
     id: int
     short_name: str
+    role: Optional[str] = None
+    permissions: Optional[List[UserAction]] = Field(None, exclude=True, repr=False)
+
+    @root_validator(pre=False, skip_on_failure=True)
+    def load_user_role(cls, values):
+        if values["role"] and values["role"] in USER_PERMISSIONS:
+            values["pemissions"] = USER_PERMISSIONS(values["role"])
+        return values
